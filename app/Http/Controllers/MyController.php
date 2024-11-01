42564343a0c61b4b9e03b6032e8e17dba4cf59c2
Shubrajit-adak-2002 <139214846+Shubrajit-adak-2002@users.noreply.github.com>
2024-11-01 18:36:56 +0530

    form_submit function added

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -46,4 +46,47 @@ class MyController extends Controller
     {
         return view('chef');
     }
+
+    function form_submit(Request $req)
+    {
+        $name = $req->input('name');
+        $email = $req->input('email');
+        $phone = $req->input('phone_number');
+        $pass = bcrypt($req->input('password'));
+        $conf_pass = $req->input('confirm_password');
+        $dob = $req->input('date_of_birth');
+
+        if ($req->file('profile_photo')) {
+            $image = $req->file('profile_photo');
+            $fileName = time() . "_" . $image->getClientOriginalName();
+            $upload = "./upload";
+            $image->move($upload, $fileName);
+            $imagePath = $upload . "/" . $fileName;
+        } else {
+            $imagePath = null; // Set a default value if no file uploaded
+        }
+
+        $state = $req->input('state');
+        $country = $req->input('country');
+        $gender = $req->input('gender');
+        $role = $req->input('role');
+
+        $data = [
+            'name' => $name,
+            'email' => $email,
+            'phone' => $phone,
+            'password' => $pass,
+            'confirm_pass' => $conf_pass,
+            'DOB' => $dob,
+            'image' => $imagePath,
+            'state' => $state,
+            'country' => $country,
+            'gender' => $gender,
+            'role' => $role
+        ];
+
+        DB::table('reg_table')->insert($data);
+
+        return view('form')->with('message', 'Data inserted');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('reg',[MyController::class,'reg_form']);
+Route::get('login',[MyController::class,'login_form']);
+Route::get('recipe',[MyController::class,'recipe_submission']);
+Route::get('appoint',[MyController::class,'appointment_form']);
+Route::get('admin',[MyController::class,'admin_table']);
+Route::get('user',[MyController::class,'user_table']);
+Route::get('chef',[MyController::class,'chef_table']);
+Route::post('submit',[MyController::class,'form_submit']);
+
